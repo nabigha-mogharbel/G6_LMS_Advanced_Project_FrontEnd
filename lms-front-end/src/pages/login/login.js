1ff6638868b2644896c39c9cd3440a534e3807f3
ahmadelhalabi <ahmdd.el.halabi.4@gmail.com>
2023-03-11 15:21:45 +0200

    NothingImportant

--- lms-front-end/src/pages/login/login.js
+++ lms-front-end/src/pages/login/login.js
@@ -7,21 +7,22 @@ import image from "../../logggo.png";
 export default function LoginPage() {
   //const [email, setEmail] = useState("");
   //const [password, setPassword] = useState("");
-  let email=useRef()
-  let password=useRef();
+  let email = useRef();
+  let password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    let data={"email" : email.current.value, "password": password.current.value}
-    console.log(data)
-    axios.post("http://127.0.0.1:8000/api/auth/login", data)
-      .then(response => {
+    let data = { email: email.current.value, password: password.current.value };
+    console.log(data);
+    axios
+      .post("http://127.0.0.1:8000/api/auth/login", data)
+      .then((response) => {
         const cookies = new Cookies();
         cookies.set("access_token", response.data.access_token, { path: "/" });
         console.log("Token saved to cookie:", response.data.access_token);
         // redirect to the dashboard or the next page after successful login
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Error logging in:", error);
         // show error message to the user
       });
@@ -37,10 +38,20 @@ export default function LoginPage() {
           </div>
           <form className="login-form-content" onSubmit={handleSubmit}>
             <div className="login-form-email">
-              <input type="email" id="email" placeholder="you@example.com"  ref={email} />
+              <input
+                type="email"
+                id="email"
+                placeholder="you@example.com"
+                ref={email}
+              />
             </div>
             <div className="login-form-password">
-              <input type="password" id="password" placeholder="Enter 6 Caracters or more" ref={password} />
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter 6 Caracters or more"
+                ref={password}
+              />
             </div>
             <div className="login-form-button">
               <button>Login</button>
--- lms-front-end/src/pages/students/student.js
+++ lms-front-end/src/pages/students/student.js
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
 export default function Students() {
-  return <div>students</div>;
+  let first_Name = useRef();
+  let last_Name = useRef();
+  let email = useRef();
+  let password = useRef();
+  let picture = useRef();
+
+  function handleRequest(e) {
+    e.preventDefault();
+    let data = {
+      first_Name: first_Name.current.value,
+      last_Name: last_Name.current.value,
+      email: email.current.value,
+      password: password.current.value,
+      // picture: last_Name.current.value,
+    };
+    console.log(data.first_Name);
+    axios
+      .post("http://127.0.0.1:8000/api/students", data)
+      .then((response) => {
+        const cookies = new Cookies();
+        cookies.set("access_token", response.data.access_token, { path: "/" });
+        console.log("Token saved to cookie:", response.data.access_token);
+        // redirect to the dashboard or the next page after successful login
+      })
+      .catch((error) => {
+        console.log("Error logging in:", error);
+        // show error message to the user
+      });
+  }
+
+  return (
+    <div className="container">
+      <div className="student-content">
+        <div className="student-form">
+          <div className="student-form-title">
+            <h2>Welcome to the student page</h2>
+          </div>
+          <form className="student-form-content" onSubmit={handleRequest}>
+            <div className="student-form-first-name">
+              <input
+                type="first-name"
+                id="first-name"
+                placeholder="first-Name"
+                ref={first_Name}
+              />
+              <input
+                type="last_Name"
+                id="last_Name"
+                placeholder="last_Name"
+                ref={last_Name}
+              />
+              <input type="email" id="email" placeholder="email" ref={email} />
+              <input
+                type="password"
+                id="password"
+                placeholder="password"
+                ref={password}
+              />
+              {/* <label for="photo">Upload your photo:</label>
+                <input type="file" id="photo" name="photo"> */}
+              <div className="login-form-button">
+                <button>Login</button>
+              </div>
+            </div>
+          </form>
+
+          {/* <div className="login-image">
+                  <img src={image} alt="Logo" />
+                </div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
