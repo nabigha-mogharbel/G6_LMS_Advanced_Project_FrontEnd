7dcaac12683edf84ae9dec122e8a21068e3c4523
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-03-11 21:54:49 +0200

    classes table

--- lms-front-end/src/App.js
+++ lms-front-end/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Classes from './pages/classes/classes';
+
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Classes></Classes>
+        
+      
     </div>
   );
 }
