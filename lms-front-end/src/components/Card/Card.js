ea5e62552ca19f93f4c0c78247419aa8cb9094c6
ahmadelhalabi <ahmdd.el.halabi.4@gmail.com>
2023-03-15 08:45:01 +0200

    ...

--- lms-front-end/src/components/Card/Card.js
+++ lms-front-end/src/components/Card/Card.js
@@ -1,10 +1,19 @@
-import "./Card.css"
+import "./Card.css";
 import { useState } from "react";
-import User from "../user"
-export default function Card(props){
-    const [logoColor, setLogoColor]=useState("#E9E3FE");
-    function focus(){
-        logoColor==="#E9E3FE"?setLogoColor("#FFF"):setLogoColor("#E9E3FE");
-    }
-    return(<div className="card" style={{backgroundColor:props.color}} onMouseEnter={focus} onMouseLeave={focus}><User color={logoColor} className="card-pic"/> <p>{props.title}</p></div>)
+import User from "../user";
+export default function Card(props) {
+  const [logoColor, setLogoColor] = useState("#E9E3FE");
+  function focus() {
+    logoColor === "#E9E3FE" ? setLogoColor("#FFF") : setLogoColor("#E9E3FE");
+  }
+  return (
+    <div
+      className="card"
+      style={{ backgroundColor: props.color }}
+      onMouseEnter={focus}
+      onMouseLeave={focus}
+    >
+      <User color={logoColor} className="card-pic" /> <p>{props.title}</p>
+    </div>
+  );
 }
